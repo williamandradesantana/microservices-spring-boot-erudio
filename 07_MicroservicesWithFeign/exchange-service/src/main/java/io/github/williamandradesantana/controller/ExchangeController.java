8a82f6792f3abc6e728cbe6759889cd2b6e56e1c
William Santana <williamandrade1058@gmail.com>
2026-04-23 12:16:06 -0300

    Retornando Mocks e Testando o Exchange Service com Spring Cloud e Spring Boot

---
+++ 07_MicroservicesWithFeign/exchange-service/src/main/java/io/github/williamandradesantana/controller/ExchangeController.java
@@ -0,0 +1,26 @@
+package io.github.williamandradesantana.controller;
+
+import io.github.williamandradesantana.model.Exchange;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/exchange-service")
+public class ExchangeController {
+
+    @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Exchange> getExchange(
+            @PathVariable("amount") BigDecimal amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    ) {
+        Exchange exchange = new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "8000");
+        return ResponseEntity.ok().body(exchange);
+    }
+}
---
+++ 07_MicroservicesWithFeign/exchange-service/src/main/java/io/github/williamandradesantana/model/Exchange.java
@@ -0,0 +1,95 @@
+package io.github.williamandradesantana.model;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Exchange implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String environment;
+
+    public Exchange(){}
+    public Exchange(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public BigDecimal getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(BigDecimal convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Exchange exchange = (Exchange) o;
+        return Objects.equals(id, exchange.id) && Objects.equals(from, exchange.from) && Objects.equals(to, exchange.to) && Objects.equals(conversionFactor, exchange.conversionFactor) && Objects.equals(convertedValue, exchange.convertedValue) && Objects.equals(environment, exchange.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(from);
+        result = 31 * result + Objects.hashCode(to);
+        result = 31 * result + Objects.hashCode(conversionFactor);
+        result = 31 * result + Objects.hashCode(convertedValue);
+        result = 31 * result + Objects.hashCode(environment);
+        return result;
+    }
+}
