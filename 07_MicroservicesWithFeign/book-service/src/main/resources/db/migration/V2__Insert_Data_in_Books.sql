8eba78f7d3bed18b9e9759dde9d0ddb94e9a7cb6
William Santana <williamandrade1058@gmail.com>
2026-04-24 17:18:14 -0300

    Adicionando o Flyway e Configurando a Conexão com o Banco de Dados

---
+++ 07_MicroservicesWithFeign/book-service/src/main/resources/db/migration/V2__Insert_Data_in_Books.sql
@@ -0,0 +1,16 @@
+INSERT INTO `book` (`author`, `launch_date`, `price`, `title`) VALUES
+('Michael C. Feathers', '2017-11-29 13:50:05.878000', 8.57, 'Working Effectively with Legacy Code'),
+('Ralph Johnson, Erich Gamma, John Vlissides e Richard Helm', '2017-11-29 15:15:13.636000', 7.87, 'Design Patterns: Elements of Reusable Object-Oriented Software'),
+('Robert C. Martin', '2009-01-10 00:00:00.000000', 13.46, 'Clean Code'),
+('Douglas Crockford', '2017-11-07 15:09:01.674000', 11.71, 'JavaScript: The Good Parts'),
+('Steve McConnell', '2017-11-07 15:09:01.674000', 10.14, 'Code Complete'),
+('Martin Fowler e Kent Beck', '2017-11-07 15:09:01.674000', 15.38, 'Refactoring: Improving the Design of Existing Code'),
+('Eric Freeman, Elisabeth Freeman, Kathy Sierra, Bert Bates', '2017-11-07 15:09:01.674000', 19.23, 'Head First Design Patterns'),
+('Eric Evans', '2017-11-07 15:09:01.674000', 16.09, 'Domain-Driven Design'),
+('Brian Goetz e Tim Peierls', '2017-11-07 15:09:01.674000', 13.99, 'Java Concurrency in Practice'),
+('Susan Cain', '2017-11-07 15:09:01.674000', 21.51, 'Quiet: The Power of Introverts in a World That Cant Stop Talking'),
+('Roger S. Pressman', '2017-11-07 15:09:01.674000', 9.79, 'Software Engineering: A Practitioners Approach'),
+('Viktor Mayer-Schonberger e Kenneth Cukier', '2017-11-07 15:09:01.674000', 9.44, 'Big Data: Como extrair volume, variedade, velocidade e valor da avalanche de informação cotidiana'),
+('Richard Hunter e George Westerman', '2017-11-07 15:09:01.674000', 16.61, 'O verdadeiro valor de TI'),
+('Marc J. Schiller', '2017-11-07 15:09:01.674000', 7.87, 'The 11 Secrets of Highly Influential IT Leaders'),
+('Aguinaldo Aragon Fernandes e Vladimir Ferraz de Abreu', '2017-11-07 15:09:01.674000', 9.44, 'Implantando a governança de TI');
