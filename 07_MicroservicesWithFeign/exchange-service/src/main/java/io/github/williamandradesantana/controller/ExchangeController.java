d0218043f711932334c5b7f4ac46191310ac7f52
William Santana <williamandrade1058@gmail.com>
2026-04-24 13:53:27 -0300

    Removendo os Mocks do Controller

--- 07_MicroservicesWithFeign/exchange-service/src/main/java/io/github/williamandradesantana/controller/ExchangeController.java
+++ 07_MicroservicesWithFeign/exchange-service/src/main/java/io/github/williamandradesantana/controller/ExchangeController.java
@@ -2,6 +2,7 @@ package io.github.williamandradesantana.controller;
 
 import io.github.williamandradesantana.environment.InstanceInformationService;
 import io.github.williamandradesantana.model.Exchange;
+import io.github.williamandradesantana.repository.ExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +20,24 @@ public class ExchangeController {
     @Autowired
     private InstanceInformationService informationService;
 
+    @Autowired
+    private ExchangeRepository repository;
+
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Exchange> getExchange(
             @PathVariable("amount") BigDecimal amount,
             @PathVariable("from") String from,
             @PathVariable("to") String to
     ) {
-        Exchange exchange = new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, informationService.retrieveServerPort());
+        Exchange exchange = repository.findByFromAndTo(from, to);
+
+        if (exchange == null) throw new RuntimeException("Currency unsupported!");
+
+        BigDecimal conversionFactor = exchange.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        exchange.setConvertedValue(convertedValue);
+        exchange.setEnvironment(informationService.retrieveServerPort());
+
         return ResponseEntity.ok().body(exchange);
     }
 }
